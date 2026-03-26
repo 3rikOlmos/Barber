acca9b0847583d868088b7f42561f0922afaa5ce
Erik Olmos <erikolmos18002@gmail.com>
2026-03-26 14:23:19 -0600

    Change base path in Vite config to relative

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Barber/',
+  base: './',
   build: {
     outDir: 'dist',
   },
@@ -12,3 +12,5 @@ export default defineConfig({
     open: true,
   },
 })
+
+
